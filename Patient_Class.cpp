7cea49ce8bcb14ad26a94589bcbe1fe0a0813d90
Pattadon Loyprasert <pattadon2546pn@gmail.com>
2023-01-19 22:06:18 +0700

    Change infrastructure.

--- Patient_Class.cpp
+++ Patient_Class.cpp
@@ -10,77 +10,47 @@
 using namespace std;    
 using json = nlohmann::json;
 
-class Patient{
-    public: 
-        string id, birth;
-        // Constructor.
-        Patient(string personal_id, string date_of_birth)
-        {
-            id = personal_id;
-            birth = date_of_birth;
-        }
 
+class Patient{
+
+    public: 
+        Patient(){}
         // Check if the id and birth in database or not
-        bool isPatient()
-        {   
-            json data;
-            ifstream file("../HealthHQ/Patient/Patient_credential.json");
-            file>>data;
-            for (auto& element: data.items())
+        bool setName(string new_name, string personal_id, string date_of_birth)
+        {
+            json patient_data;
+            ifstream file ("../HealthHQ/Patient/Patient_credential.json");
+            file >> patient_data;
+            for (auto & element: patient_data.items())
             {
                 string key = element.key();
-                json value = element.value();
-                if (id == value["personal_id"] && birth == value["date_of_birth"])
+                json dataTable = element.value();
+                if (dataTable["personal_id"] == personal_id && dataTable["date_of_birth"] == date_of_birth)
                 {
+                    std::map<std::string, std::string> form;
+                    form["personal_id"] = dataTable["personal_id"];
+                    form["name"] = new_name;
+                    form["lastname"] = dataTable["lastname"];
+                    form["date_of_birth"] = dataTable["date_of_birth"];
+                    form["gender"] = dataTable["gender"];
+                    form["address"] = dataTable["address"];
+                    form["phone"] = dataTable["phone"];
+                    form["email"] = dataTable["email"];
+
+                    patient_data[key].update(form);  
+                        
+                    ofstream jsonFile("../HealthHQ/Patient/Patient_credential.json");
+                    jsonFile << patient_data.dump(4);
+                    jsonFile.close();
                     return true;
                 }
             }
             return false;
-        }  
-        void showAppointments()
-        {
-            json appointment_data;
-            ifstream file("../HealthHQ/Record/Appointment.json");
-            file>>appointment_data;
-            for(auto& element : appointment_data.items())
-            {
-                string num = element.key();
-                json information = element.value();
-                if (id == information["personal_id"] && birth == information["date_of_birth"])
-                {
-                    int appointment_num = 1;
-                    for (auto & sche : information["appointment"].items())
-                    {
-
-                        string key = sche.key();
-                        json sche_info = sche.value();
-                        if (sche_info["status"] == "confirmed" || sche_info["status"] == "pending")
-                        {
-
-                            cout<<"--------------------------------------------------------\n";
-                            cout<<"Your appointment number # "<<appointment_num<<" are the following\n";
-                            cout<<"--------------------------------------------------------\n";
-                            cout<<"Symptom: "<<sche_info["symptoms"]<<"\n";
-                            cout<<"Date: "<<sche_info["date"]<<"\n";
-                            cout<<"Doctor: "<<sche_info["doctor"]<<"\n";
-                            appointment_num ++ ;
-                        }
-                        else
-                        {
-                            cout<<"Sorry there's no appointment scheduled for you.";
-                        
-                        }
-                    
-                    }
-                
-                }           
-            }
         }
 
-    
 };
 int main()
 {
-    Patient Patient1("1948822365845", "31122546");
-    Patient1.showAppointments();
+    Patient Patient1;
+    cout<<Patient1.setName("Kong", "1948822365845", "31122008");
 }
